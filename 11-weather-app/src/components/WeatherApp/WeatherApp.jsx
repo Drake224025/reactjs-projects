bf5fd1b0c7924bde22482b7e8dd8785df7b2a9d2
Drake224025 <sasithamare@gmail.com>
2024-04-15 10:52:41 +0530

    style: finalize styles

--- 11-weather-app/src/components/WeatherApp/WeatherApp.jsx
+++ 11-weather-app/src/components/WeatherApp/WeatherApp.jsx
@@ -1,12 +1,12 @@
 import "./styles.css";
 // import clearIcon from "../../assets/clear.png";
-// const cloudIcon = require("../../assets/cloud.png");
-// const drizzleIcon = require("../../assets/drizzle.png");
-// const humidityIcon = require("../../assets/humidity.png");
-// const rainIcon = require("../../assets/rain.png");
+import cloudIcon from "../../assets/cloud.png";
+// import drizzleIcon fom "../../assets/drizzle.png";
+import humidityIcon from "../../assets/humidity.png";
+// import rainIcon from "../../assets/rain.png";
 import searchIcon from "../../assets/search.png";
-// const snowIcon = require("../../assets/snow.png");
-// const windIcon = require("../../assets/wind.png");
+// import snowIcon from "../../assets/snow.png";
+import windIcon from "../../assets/wind.png";
 
 export default function WeatherApp() {
   return (
@@ -15,6 +15,27 @@ export default function WeatherApp() {
         <input type="text" className="city-input" placeholder="Search" />
         <div className="search-icon">
           <img src={searchIcon} alt="search icon" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloudIcon} alt="cloud icon" />
+      </div>
+      <div className="weather-temp">24℃</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidityIcon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={windIcon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">18 kmh</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
       </div>
     </div>
